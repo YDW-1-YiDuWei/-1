93890256c6079621a1c142ced543332403c231cf
易都伟 易 <2669872468@qq.com>
2021-02-22 20:06:39 +0800

    修改（21）

--- 登录界面.cs
+++ 登录界面.cs
@@ -19,7 +19,7 @@ namespace 点餐系统
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //1
+            
             首页 sy = new 首页();
             sy.Show();
             this.Hide();
