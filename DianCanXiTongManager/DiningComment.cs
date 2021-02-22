3c358eb8f92ab817d89b1d2d952e0f49588e435a
易都伟 易 <2669872468@qq.com>
2021-02-22 20:05:25 +0800

    修改（2）

---
+++ DianCanXiTongManager/DiningComment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DianCanXiTongManager
+{
+    public class DiningComment
+    {
+        public int id { get; set; }
+        public int RestaurantId { get; set; }
+        public string DiningComments { get; set; }
+        public double DiningGrade { get; set; }
+        public int ClientId { get; set; }
+    }
+}
