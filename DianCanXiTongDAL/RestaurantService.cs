dd561bcd26a8aa208cd3cdfabe4f4fa462792111
Administrator <Administrator@>
2021-02-23 09:57:40 +0800

    修改

--- DianCanXiTongDAL/RestaurantService.cs
+++ DianCanXiTongDAL/RestaurantService.cs
@@ -8,6 +8,12 @@ namespace DianCanXiTongDAL
 {
     public class RestaurantService//餐馆数据层
     {
-
+        /// <summary>
+        /// 查询餐馆全部
+        /// </summary>
+        public void InquireRestaurant() 
+        {
+        
+        }
     }
 }
