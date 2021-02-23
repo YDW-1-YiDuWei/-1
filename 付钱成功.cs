f9c8c862f491baa064ffac8e12062e8b79cb2dbd
2774106053 <2774106053@qq.com>
2021-02-23 09:53:32 +0800

    3333

--- 付钱成功.cs
+++ 付钱成功.cs
@@ -20,6 +20,7 @@ namespace 点餐系统
         private void button1_Click(object sender, EventArgs e)
         {
             //
+            //
             提交 tj = new 提交();
             tj.Show();
             this.Close();
