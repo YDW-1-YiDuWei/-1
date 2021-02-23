93044dbaad87e4513ea02f756d6e62fe3a4b0278
Administrator <Administrator@>
2021-02-23 19:58:00 +0800

    没什么

--- DianCanXiTongDAL/ReservationService.cs
+++ DianCanXiTongDAL/ReservationService.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DianCanXiTongManager;
 
 namespace DianCanXiTongDAL
 {
     /// <summary>
-    /// 订单
+    /// 订单数据层
     /// </summary>
     public class ReservationService
     {
-        string str = "";
+        DBHelper db = new DBHelper();
+        public void InquireReservation() //查询订单信息
+        {
+            string sql = "";
+        }
     }
 }
