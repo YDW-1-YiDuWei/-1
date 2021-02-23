e12e6cde1b190f79a639d516b8d225463dfb2049
易都伟 易 <2669872468@qq.com>
2021-02-23 11:31:14 +0800

    1

--- 订单.Designer.cs
+++ 订单.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // 订单
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(734, 450);
+            this.Name = "订单";
             this.Text = "订单";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
