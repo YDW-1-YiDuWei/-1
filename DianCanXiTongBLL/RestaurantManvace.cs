30a869d8695ce338a6200761c3cadece992f78e0
Administrator <Administrator@>
2021-02-23 14:39:46 +0800

    添加（餐馆业务层）

--- DianCanXiTongBLL/RestaurantManvace.cs
+++ DianCanXiTongBLL/RestaurantManvace.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DianCanXiTongDAL;
+using DianCanXiTongManager;
 
-namespace DianCanXiTongDAL
+namespace DianCanXiTongBLL
 {
-    public class Class1
+    public class RestaurantManvace
     {
+
     }
 }
