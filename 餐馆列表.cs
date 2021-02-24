596c5f5d260d02f0c407ffc9b4c2ed8c64369564
Administrator <Administrator@>
2021-02-24 08:57:26 +0800

    12345678986

--- 餐馆列表.cs
+++ 餐馆列表.cs
@@ -63,7 +63,7 @@ namespace 点餐系统
 
         private void lvSJXX_Click(object sender, EventArgs e)//单击里面的餐馆的时候
         {
-            if (lvSJXX.SelectedItems.Count != 0)
+            if (lvSJXX.SelectedItems.Count != 0&&lvSJXX.SelectedItems.ToString()!= "餐馆名字：" + "    餐馆地址：" + "     餐馆电话：")
             {
                 MessageBox.Show("1");
             }
