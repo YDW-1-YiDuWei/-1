085ae7e3c560aca222ccfa1731d9361dcdb43d5b
易都伟 易 <2669872468@qq.com>
2021-02-24 10:27:19 +0800

    用户菜品查询，唐梦石

--- DianCanXiTongBLL/CuisineInformationsManager.cs
+++ DianCanXiTongBLL/CuisineInformationsManager.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 
+using DianCanXiTongDAL;
+using DianCanXiTongManager;
 
 namespace DianCanXiTongBLL
 {
     public class CuisineInformationsManager
     {
-        public void CuisinelnformationsSelectManager(string leix)
+        private CuisinelnformationsService cS = new CuisinelnformationsService();
+        /// <summary>
+        /// 点餐查询，业务层
+        /// </summary>
+        /// <param name="leix"></param>
+        public List<CuisineInformations> CuisinelnformationsSelectManager(string leix)
         {
+            return cS.CuisinelnformationsSelectService(leix);
         }
     }
 }
