ea668ce369129180d0c50467baddde09bced6f19
易都伟 易 <2669872468@qq.com>
2021-02-24 20:34:29 +0800

    订单增加

---
+++ DianCanXiTongBLL/DistributionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DianCanXiTongDAL;
+
+namespace DianCanXiTongBLL
+{
+    public class DistributionManager
+    {
+        /// <summary>
+        /// 订单增加，业务层
+        /// </summary>
+        /// <returns></returns>
+        public int DistributionAddManager(string riderId, string cuisineId, string lientId, string restaurantId)
+        {
+            DistributionService distribution = new DistributionService();
+            return distribution.CuisinelnformationsAddService(riderId, cuisineId, lientId, restaurantId);
+        }
+    }
+}
--- DianCanXiTongDAL/DistributionService.cs
+++ DianCanXiTongDAL/DistributionService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
+
 namespace DianCanXiTongDAL
 {
     public class DistributionService
