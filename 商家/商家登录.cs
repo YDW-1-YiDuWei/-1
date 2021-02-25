7a887175139f0f594c4723f9782bef2e38ce7477
易都伟 易 <2669872468@qq.com>
2021-02-25 20:28:48 +0800

    123

--- 商家/商家登录.cs
+++ 商家/商家登录.cs
@@ -19,7 +19,7 @@ namespace 点餐系统
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            商家首页 a = new 商家首页();
         }
     }
 }
--- 商家/商家首页.cs
+++ 商家/商家首页.cs
@@ -16,10 +16,5 @@ namespace 点餐系统.商家
         {
             InitializeComponent();
         }
-
-        private void toolStripLabel2_Click(object sender, EventArgs e)//商家首页按钮
-        {
-            panel1.Visible = true;//显示首页
-        }
     }
 }
