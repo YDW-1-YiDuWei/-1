77f01fa68de8a638c628c8d315a4f2de696242dd
易都伟 易 <2669872468@qq.com>
2021-02-26 14:58:58 +0800

    xxxxx

--- 商家/商家首页.cs
+++ 商家/商家首页.cs
@@ -56,12 +56,25 @@ namespace 点餐系统
             return true;
         }
 
+        private void toolStripLabel2_Click(object sender, EventArgs e)//商家首页（按钮）
+        {
+            panel1.Visible = true;//商家首页显示（panel1）
+            panel2.Visible = false;//商家的菜品查询（panel2）
+            panel3.Visible = false;//商家的菜品增加/修改（panel3）
+        }
+
+        private void toolStripLabel3_Click(object sender, EventArgs e)//商家我的（按钮）
+        {
+            panel1.Visible = false;//商家首页显示（panel1）
+            panel2.Visible = false;//商家的菜品查询（panel2）
+            panel3.Visible = false;//商家的菜品增加/修改（panel3）
+        }
         private void 商家首页_Load(object sender, EventArgs e)
         {
             if (list != null)
             {
-                label1.Text="商家的名称"+ list[0].RestaurantName;
-                pictureBox3.Image = Image.FromFile(list[0].RestaurantImage);
+                /*label1.Text="商家的名称"+ list[0].RestaurantName;
+                pictureBox3.Image = Image.FromFile(list[0].RestaurantImage);*/
             }
         }
     }
