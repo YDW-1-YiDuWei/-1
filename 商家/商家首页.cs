2b2864f77a3134e096aa7c6f9d3b87a8e4edd77b
易都伟 易 <2669872468@qq.com>
2021-02-26 15:06:30 +0800

    123

--- 商家/商家首页.cs
+++ 商家/商家首页.cs
@@ -61,6 +61,7 @@ namespace 点餐系统
             panel1.Visible = true;//商家首页显示（panel1）
             panel2.Visible = false;//商家的菜品查询（panel2）
             panel3.Visible = false;//商家的菜品增加/修改（panel3）
+
         }
 
         private void toolStripLabel3_Click(object sender, EventArgs e)//商家我的（按钮）
