c71666487fdff3f2f590d82276f626e7e22302d7
Administrator <Administrator@>
2021-02-27 14:53:49 +0800

    修改了，修改菜品时必须选择图片的bug。

--- Temp.cs
+++ Temp.cs
@@ -12,5 +12,9 @@ namespace 点餐系统
         /// 餐馆图片路径
         /// </summary>
         public static string pathCG = @"C:\菜谱\";
+        /// <summary>
+        /// 判断菜品修改重新选择图片没有
+        /// </summary>
+        public static int index = 0;
     }
 }
