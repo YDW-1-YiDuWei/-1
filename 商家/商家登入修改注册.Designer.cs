076c179a749622ec05d5e168dee306df413b16ec
易都伟 易 <2669872468@qq.com>
2021-03-01 11:32:41 +0800

    123

--- 商家/商家登入修改注册.Designer.cs
+++ 商家/商家登入修改注册.Designer.cs
@@ -52,7 +52,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(36, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 14);
+            this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "商家名称：";
             // 
@@ -61,7 +61,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(36, 238);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 14);
+            this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 0;
             this.label2.Text = "商家地址：";
             // 
@@ -70,7 +70,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(60, 126);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 14);
+            this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 0;
             this.label3.Text = "账号：";
             // 
@@ -79,7 +79,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(60, 154);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 12);
+            this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 0;
             this.label4.Text = "密码：";
             // 
@@ -88,7 +88,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(36, 182);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 14);
+            this.label5.Size = new System.Drawing.Size(65, 12);
             this.label5.TabIndex = 0;
             this.label5.Text = "确认密码：";
             // 
@@ -97,7 +97,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(36, 210);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 14);
+            this.label6.Size = new System.Drawing.Size(65, 12);
             this.label6.TabIndex = 0;
             this.label6.Text = "商家电话：";
             // 
