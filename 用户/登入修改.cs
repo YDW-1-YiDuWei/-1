4972f46330c89bedeb316ec52da9680649ef79b0
张云峰 <2836459178@qq.com>
2021-03-02 19:50:43 +0800

    1

--- 用户/登入修改.cs
+++ 用户/登入修改.cs
@@ -60,7 +60,7 @@ namespace 点餐系统
 
         private void Button1_Click_1(object sender, EventArgs e)//确认按钮
         {
-            if (txtPwd2.Text.Trim() != txtPwd.Text.Trim())
+            if (txtPwd2.Text.Trim() != txtPwd.Text.Trim())//
             {
                 MessageBox.Show("密码不一致", "登录提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
