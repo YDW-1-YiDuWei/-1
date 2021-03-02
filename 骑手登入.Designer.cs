5c852ba7e0fd3ca411d0dba3ecacb953744a0dec
易都伟 易 <2669872468@qq.com>
2021-03-02 21:10:46 +0800

    123

---
+++ 骑手登入.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace 点餐系统
+{
+    partial class 骑手登入
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(骑手登入));
+            this.SuspendLayout();
+            // 
+            // 骑手登入
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1188, 596);
+            this.Name = "骑手登入";
+            this.Text = "骑手登入";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
