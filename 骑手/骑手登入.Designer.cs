cd18a651b892bb69aefd7a120d53c403241b50e1
Administrator <Administrator@>
2021-03-03 19:46:34 +0800

    骑手登录里面的图片控件位置微调

--- 骑手/骑手登入.Designer.cs
+++ 骑手/骑手登入.Designer.cs
@@ -45,7 +45,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(80, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(102, 20);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(120, 120);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
