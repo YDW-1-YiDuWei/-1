f7cbccf6d9bea7bb8ad804b86d2b5b810cee720d
Administrator <Administrator@>
2021-03-04 17:09:00 +0800

    骑手登录密码框显示⭐

--- 骑手/骑手登入.Designer.cs
+++ 骑手/骑手登入.Designer.cs
@@ -142,7 +142,7 @@
             this.label3.Location = new System.Drawing.Point(506, 407);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 12);
-            this.label3.TabIndex = 5;
+            this.label3.TabIndex = 4;
             this.label3.Text = "注册";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
